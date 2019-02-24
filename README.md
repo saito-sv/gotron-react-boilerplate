db3aaa65ece3fa0a657a2020a6333b36bffd068e
Marlon Monroy <marlon@Marlons-MBP.hsd1.ca.comcast.net>
2019-02-23 22:05:07 -0800

    webpack

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gotron-react-boilerplate
-Gotron electronjs webpack 4 boilerplate
+
+##Gotron electronjs webpack 4 boilerplate
+Using react 16.7.alpha2 for hooks
---
+++ ui/webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg|mp4)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {}
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
